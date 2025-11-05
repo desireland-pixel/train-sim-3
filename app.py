4bbf480b94a3c83c04055831a724a8d366afd860
desireland-pixel <desireland13@gmail.com>
2025-11-05 18:51:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ train_positions = compute_train_positions(trains, time) if not trains.empty else
 # Page title
 # -------------------------
 st.title("🚉 Train–Warehouse Simulation")
-st.markdown(f"**Simulation Time: {current_time} min**")
+st.markdown(f"**Simulation Time: {time} min**")
 
 # -------------------------
 # DIGITAL CLOCK ABOVE GRAPH
