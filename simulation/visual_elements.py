c150c5e31768e04f339a8d70eed39076cf9c5bf3
desireland-pixel <desireland13@gmail.com>
2025-11-13 22:18:27 +0100

    Update visual_elements.py

--- simulation/visual_elements.py
+++ simulation/visual_elements.py
@@ -54,7 +54,7 @@ def draw_humans(fig, human_positions):
             x=[x], y=[y],
             mode="markers+text",
             text=[person_id],
-            textposition="center",
+            textposition="top center",
             textfont=dict(color="black"),
             marker=dict(size=10, color="orange", symbol="circle"),
             name="Humans",
@@ -68,7 +68,7 @@ def draw_humans(fig, human_positions):
                 x=[x], y=[y],
                 mode="text",
                 text=[temp_label],
-                textposition="top center",
+                textposition="bottom center",
                 textfont=dict(color="blue"),
                 showlegend=False
             ))
